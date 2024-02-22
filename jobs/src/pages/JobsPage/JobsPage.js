03d5a8f23276d386612a15e229c486c4617a1214
Your Name <you@example.com>
2024-02-22 11:39:30 +0300

    starting

---
+++ jobs/src/pages/JobsPage/JobsPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+
+const JobsPage = () => {
+  return (
+    <View>
+      <Text>Jobs</Text>
+    </View>
+  );
+};
+
+export default JobsPage;
