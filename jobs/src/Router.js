277ac0fcdf0c146976a81d5184778c46bc82b0ea
Your Name <you@example.com>
2024-02-23 11:29:25 +0300

    navigation

--- jobs/src/Router.js
+++ jobs/src/Router.js
@@ -1,8 +1,19 @@
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import React from 'react';
-import {View} from 'react-native';
+
+import JobsPage from './pages/JobsPage';
+
+const Stack = createNativeStackNavigator();
 
 const Router = () => {
-  return <View>Router</View>;
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Jobs" component={JobsPage} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 };
 
 export default Router;
