75b2a73b7aee40f0bcf5cf4ece6a3c45e3a6e832
Your Name <you@example.com>
2024-02-27 12:07:27 +0300

    flatlist data

---
+++ jobs/src/components/JobsCard/JobsCard.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+
+const JobsCard = () => {
+  return (
+    <View>
+      <Text></Text>
+      <Text></Text>
+      <Text></Text>
+      <Text></Text>
+    </View>
+  );
+};
+
+export default JobsCard;
--- jobs/src/pages/JobsPage/JobsPage.js
+++ jobs/src/pages/JobsPage/JobsPage.js
@@ -1,13 +1,29 @@
 import React from 'react';
-import {FlatList, Text, View} from 'react-native';
+import {FlatList, View} from 'react-native';
 import useFetch from '../../hooks/useFetch';
+import Loading from '../../components/Loading';
+import Error from '../../components/Error';
+import Config from 'react-native-config';
 
 const JobsPage = () => {
-  const {data, error, loading} = useFetch();
+  const [page, setPage] = useState(0);
+  const {data, error, loading} = useFetch(`${Config.API_URL}${page}`);
+
+  if (error) {
+    return <Error />;
+  }
+
+  if (loading) {
+    return <Loading />;
+  }
+
+  const renderJobs = () => (
+    
+  )
 
   return (
     <View>
-      <FlatList />
+      <FlatList data={data.results} renderItem={renderJobs} />
     </View>
   );
 };
