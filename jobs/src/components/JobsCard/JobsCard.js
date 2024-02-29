685e9766864867ef4dc021b43608f26abf1e94fa
Your Name <you@example.com>
2024-02-29 11:06:08 +0300

    jobsCard

--- jobs/src/components/JobsCard/JobsCard.js
+++ jobs/src/components/JobsCard/JobsCard.js
@@ -5,10 +5,14 @@ import styles from './JobsCard.style';
 const JobsCard = ({jobs}) => {
   return (
     <View style={styles.container}>
-      <Text>{jobs.name}</Text>
+      <Text numberOfLines={1} style={styles.jobsname}>
+        {jobs.name}
+      </Text>
       <Text>{jobs.company.name}</Text>
       <Text>{jobs.locations[0].name}</Text>
-      <Text>{jobs.levels[0].name}</Text>
+      <View style={styles.innerContainer}>
+        <Text>{jobs.levels[0].name}</Text>
+      </View>
     </View>
   );
 };
--- jobs/src/components/JobsCard/JobsCard.style.js
+++ jobs/src/components/JobsCard/JobsCard.style.js
@@ -1,5 +1,17 @@
 import {StyleSheet} from 'react-native';
 
 export default StyleSheet.create({
-  container: {},
+  container: {
+    margin: 5,
+    borderWidth: 1,
+    borderColor: 'grey',
+    backgroundColor: 'white',
+    borderRadius: 10,
+    minHeight: 100,
+  },
+  jobsname: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    color: 'black',
+  },
 });
